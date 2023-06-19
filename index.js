ad46c21550c8f980f20825d4ae0a193c2cab4cdb
DeepCoomer <deepcoomer198@gmail.com>
2023-06-19 22:21:29 +0530

    hosting configuration

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ import express from "express";
 import cors from "cors";
 import Connection from "./database/db.js";
 import router from "./routes/routes.js";
-import bodyParser from 'body-parser';
-import users from './routes/users.js';
-import PasswordReset from './routes/passwordReset.js';
+import bodyParser from "body-parser";
+import users from "./routes/users.js";
+import PasswordReset from "./routes/passwordReset.js";
 
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT = 8000
+const PORT = process.env.PORT;
 
-app.use('/api', router);
-app.use('/api/users',users);
-app.use('/api/password-reset',PasswordReset);
+app.use("/api", router);
+app.use("/api/users", users);
+app.use("/api/password-reset", PasswordReset);
 
 Connection();
 
+app.get("/", (req, res) => {
+    res.send("Hey this is my API running 🥳");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:8000`)
-})
+  console.log(`Server is running on http://localhost:8000`);
+});
