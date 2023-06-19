f5c2b63215f0911eca543e308904c39990ab11c6
DeepCoomer <deepcoomer198@gmail.com>
2023-06-19 21:09:11 +0530

    initial commit

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const Connection = async () => {
-    const URL = "mongodb://127.0.0.1:27017/myblog";
+    // const URL = "mongodb://127.0.0.1:27017/myblog";
+    const URL = process.env.URL;
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
         console.log('Database connected successfully');
